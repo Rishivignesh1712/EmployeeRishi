01e0e6fdf42a8d8354bbf2ce263f837c80493196
Administrator <Administrator@C78784A6CB43503>
2024-02-06 12:35:41 +0530

    Now

---
+++ EmployeeDetails/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeDetails.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmployeeDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee { Name = "Rishi", Salary = 90000},
+            new Employee { Name = "prabhu", Salary = 50000},
+            new Employee { Name = "vadivel", Salary = 40000},
+            new Employee { Name = "deva", Salary = 60000},
+            new Employee { Name = "bhu", Salary = 80000},
+            new Employee { Name = "varadha", Salary = 80000},
+            new Employee { Name = "parthiban", Salary = 90000},
+            new Employee { Name = "Subramani", Salary = 40000},
+            new Employee { Name = "Harold das", Salary = 50000},
+            new Employee { Name = "Elisa", Salary = 60000}
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(int inputSalary)
+        {
+            var filteredEmployees = employees.FindAll(e => e.Salary > inputSalary);
+            return View(filteredEmployees);
+        }
+    }
+}
---
+++ EmployeeDetails/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeDetails.Models
+{
+    public class Employee
+    {
+       public string Name {  get; set; }
+        public int Salary { get; set; }
+    }
+}
